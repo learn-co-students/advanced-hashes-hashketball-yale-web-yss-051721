217af47858dbcc63e14501ffecd16ddb840813f9
Bill Feng <bill.feng@yale.edu>
2021-05-25 06:35:16 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -125,5 +125,104 @@ def game_hash
     }
   }
 end
-
 # Write code here
+
+require 'pry'
+def get_all_player_data(game)
+  playerData=[]
+  game.each{
+    |location, teamData|
+    playerData=playerData+teamData[:players]
+  }
+  playerData
+end
+  
+def num_points_scored(player_name)
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.find{|profile| profile[:player_name]==player_name}
+  return profile[:points]
+end
+
+def shoe_size(player_name)
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.find{|profile| profile[:player_name]==player_name}
+  return profile[:shoe]
+end
+
+def team_colors(teamName)
+  teamProfile=game_hash.find{|key,teamInfo|teamInfo[:team_name]==teamName}
+  return teamProfile[1][:colors]
+end
+
+def team_names
+  teamNames=[]
+  game_hash.each{
+    |location,teamData|
+    teamNames << teamData[:team_name]
+  }
+  teamNames
+end
+
+def player_numbers(team_name)
+    teamProfile=game_hash.find{|key,teamInfo|teamInfo[:team_name]==team_name}
+    playerNums=teamProfile[1][:players].collect{
+      |playerProfile|
+      playerProfile[:number]
+    }
+    playerNums
+end
+
+def player_stats(player_name)
+  playerData=get_all_player_data(game_hash)
+  playerData.find{|profile| profile[:player_name]==player_name}
+end
+
+def big_shoe_rebounds
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.max{|profile1, profile2| profile1[:shoe] <=> profile2[:shoe]}
+  #binding.pry
+  return profile[:rebounds]
+end
+
+def most_points_scored
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.max{|profile1, profile2| profile1[:points] <=> profile2[:points]}
+  #binding.pry
+  return profile[:player_name]
+end
+
+def sum_points_of_teamData(teamData)
+  players=teamData[:players]
+  players.reduce(0){|sum,playerProfile| sum+playerProfile[:points]}
+end
+
+def winning_team
+  winning=""
+  score=0
+  pointTrack=game_hash.map{
+    |location, teamData|
+    points=sum_points_of_teamData(teamData)
+    [teamData[:team_name],points]
+  }
+  p pointTrack
+  maxPair=pointTrack.max{|t1,t2| t1[1] <=> t2[1]}
+  maxPair[0]
+end
+
+def player_with_the_longest_name
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.max{|profile1, profile2| profile1[:player_name].length <=> profile2[:player_name].length}
+  #binding.pry
+  return profile[:player_name]
+end
+
+def most_steals
+  playerData=get_all_player_data(game_hash)
+  profile=playerData.max{|profile1, profile2| profile1[:steals] <=> profile2[:steals]}
+  #binding.pry
+  return profile[:player_name]
+end
+
+def long_name_steals_a_ton?
+  return (player_with_the_longest_name==most_steals)
+end
