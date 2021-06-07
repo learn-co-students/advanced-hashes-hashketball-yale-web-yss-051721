5c1b7f321dd4028ac291ed799c204450bd64f00d
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-06-07 20:52:00 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require "pry"
 def game_hash
   {
     home: {
@@ -127,3 +128,77 @@ def game_hash
 end
 
 # Write code here
+
+def home_team
+  game_hash[:home]
+  
+end
+
+def home_players
+  home_team[:players]
+end
+
+def get_all_players
+  home_players + game_hash[:away][:players]
+end
+
+def find_player(name)
+ get_all_players.find { |player_hash| player_hash[:player_name] == name }
+end 
+
+def find_team(team_name)
+  game_hash.values.find do |team|
+    team[:team_name] == team_name
+    #binding.pry
+  end
+end
+
+def num_points_scored(name)
+ #player = get_all_players.find { |player_hash| player_hash[:player_name] == name }
+ #above is the syntax for searching for a player in the hash with a given name
+ 
+ find_player(name)[:points]
+ 
+# binding.pry
+end
+#binding.pry
+
+def shoe_size(name)
+ player = get_all_players.find { |player_hash| player_hash[:player_name] == name }
+ #above is the syntax for searching for a player in the hash with a given name
+ player[:shoe]
+# binding.pry
+end
+def team_names
+  #iterate over game hash
+  #we wanto pull ut team_name from eah team hash
+  values_arr = game_hash.values #the values additon will give us the hash
+  names_arr = values_arr.map do |team_hash|
+    team_hash[:team_name]
+end
+  binding.pry
+end
+
+def team_colors(team_name)
+  #find team hash
+team_hash = find_team(team_name)
+#grabs colors from the team
+team_hash[:colors]
+end
+
+
+def player_numbers(team_name)
+  find_team(team_name)[:players].map {|each_player| player_hash[:number] }
+  
+  
+  binding.pry
+end
+
+
+def player_stats(name)
+  
+
+end
+
+
+
